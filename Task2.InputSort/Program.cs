410388e588c089f77d2191794b1388bb6ce07fbb
vrq13 <vrq.ykt@gmail.com>
2026-09-16 13:52:18 +0300

    Update Program.cs

--- Task2.InputSort/Program.cs
+++ Task2.InputSort/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
