74235d1e9d07a950b1d5f02229829ff62b577636
vrq13 <vrq.ykt@gmail.com>
2026-09-16 13:51:58 +0300

    Update Program.cs

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,37 +1,11 @@
-﻿using System;
-using System.Linq;
-
-class Program
+using System;
+namespace Task1
 {
-    static void Main()
+    class Program
     {
-        // 1. Создание массива из 10 случайных чисел в диапазоне [1, 100]
-        Random random = new Random();
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.Length; i++)
+        static void Main()
         {
-            numbers[i] = random.Next(1, 101); // 101 не включается
+            Console.WriteLine("Hello World!");
         }
-
-        // 2. Расчет необходимых метрик
-        long sum = numbers.Sum();
-        
-        // Для произведения используем double, чтобы избежать переполнения и получить вывод в экспоненциальном формате (E)
-        double product = 1;
-        foreach (int num in numbers)
-        {
-            product *= num;
-        }
-
-        double average = numbers.Average();
-        int evenCount = numbers.Count(n => n % 2 == 0);
-        int greaterThanAverageCount = numbers.Count(n => n > average);
-
-        // 3. Вывод результатов в консоль
-        Console.WriteLine($"Массив: {string.Join(", ", numbers)}");
-        Console.WriteLine($"Сумма: {sum}");
-        Console.WriteLine($"Произведение: {product:0.00E+00}");
-        Console.WriteLine($"Чётных чисел: {evenCount}");
-        Console.WriteLine($"Больше среднего ({average:0.0}): {greaterThanAverageCount}");
     }
 }
